3f54f7c6bf8a1a0dddd94f59d09ae3cd085111a6
saketh <sakethramansundaram78@gmail.com>
2023-03-14 20:16:10 +0530

    refactor: ProcessorFactory into separate class

--- src/build_index/base_processor.py
+++ src/build_index/base_processor.py
@@ -7,28 +7,6 @@ import nltk
 class BaseProcessor(ABC):
     lemmatizer = nltk.stem.WordNetLemmatizer()
 
-    @staticmethod
-    def get_processor(extension: str):
-        from src.build_index.processors.docx_processor import DocxProcessor
-        from src.build_index.processors.image_processor import ImageProcessor
-        from src.build_index.processors.pdf_processor import PdfProcessor
-        from src.build_index.processors.text_processor import TextProcessor
-
-        PROCESSOR_MAPPING = {
-            ".txt": TextProcessor(),
-            ".docx": DocxProcessor(),
-            ".png": ImageProcessor(),
-            ".jpeg": ImageProcessor(),
-            ".ppm": ImageProcessor(),
-            ".tiff": ImageProcessor(),
-            ".bmp": ImageProcessor(),
-            ".pdf": PdfProcessor(),
-        }
-        try:
-            return PROCESSOR_MAPPING[extension]
-        except KeyError:
-            raise KeyError("Unsupported file extension provided.")
-
     def preprocessed_tokens(self, file_path: str):
         doc_contents = self.parse_doc(file_path)
         tokens = self.tokenize(doc_contents)
